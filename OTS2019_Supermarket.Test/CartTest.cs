21b928fcd5dfa27864bc91b09533855a275fd03d
nemanjajancic-IT-33-2024 <nemanjajancic45@gmail.com>
2026-04-16 19:51:09 +0200

    IT33-2024

--- OTS2019_Supermarket.Test/CartTest.cs
+++ OTS2019_Supermarket.Test/CartTest.cs
@@ -8,8 +8,17 @@ using System.Threading.Tasks;
 
 namespace OTS_Supermarket.Test
 {
+    [TestFixture]
     public class CartTest
     {
-
+    [Test]
+    public void AddOneToCart_ShouldAdditemToCart_Success()
+    {
+        Cart cart = new Cart()
+        Monitor monitor = new Monitor();
+            cart.AddOneToCart(monitor);
+            Assert.That(cart.Size.Is.EqualTo(1));
+            Assert.That(cart.Amount.Is.EqualTo(100));
+    }
     }
 }
